99323de64a84add81a4ee2fc4244dd61b7e0f218
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-11-01 09:24:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function binarySearch(nums, target) {
-// Your code here
-// Return index if target is present
-// else return -1
+  let low = 0;
+  let high = nums.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+    else if (nums[mid] < target) {
+      low = mid + 1;
+    }
+    else {
+      high = mid - 1;
+    }
+  }
+
+  return -1;
 }
 
 module.exports = binarySearch;
